e2439b354f59d2ab775eb1bbed773d72f8f15732
Reflection <desiderata_cpp@icloud.com>
2025-10-26 22:59:35 +0300

    база дампа с a2x

--- reflectionCS2/Program.cs
+++ reflectionCS2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace reflectionCS2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+        }
+
+   
+        public static void Log(string text, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public static void Log(int integer, ConsoleColor color = ConsoleColor.Gray) => Log(integer.ToString(), color);
+
+    }
+}
